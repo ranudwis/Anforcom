a2a503b89f37ff8fdd4087d431c2a9a37f68e45f
Ranu Dwi Setiadi <ranudwis@gmail.com>
2020-05-29 09:52:44 +0700

    Buat AdminUserSeeder

---
+++ database/seeds/AdminUserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+
+class AdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $admin = new User();
+        $admin->name = 'admin';
+        $admin->email = 'admin';
+        $admin->nim = '0';
+        $admin->password = 'admin';
+        $admin->role = 'admin';
+        $admin->ktm = '';
+        $admin->save();
+    }
+}
