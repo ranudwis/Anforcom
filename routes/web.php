5417cd63e06a500f4ef51baf8141f9b4aa10be6f
Ranu Dwi Setiadi <ranudwis@gmail.com>
2020-05-30 20:41:49 +0700

    Typo

--- routes/web.php
+++ routes/web.php
@@ -38,7 +38,8 @@ Route::prefix('/admin')
         Route::get('/pembayaran/{team}/konfirmasi', 'PaymentController@confirm')->name('payment.confirm');
         Route::get('/pembayaran/{team}/tolak', 'PaymentController@reject')->name('payment.reject');
         Route::get('/pembayaran/{team}/hapus', 'PaymentController@delete')->name('payment.delete');
-        Route::get('/pembayaran/{team}/hapus', 'PaymentController@delete')->name('paymenet.delete');
+        Route::get('/pembayaran/{team}/hapus', 'PaymentController@delete')->name('payment.delete');
+
         Route::view('/sponsor', 'admin.partial.sponsor')->name('sponsor');
         Route::view('/medpart', 'admin.partial.medpart')->name('medpart');
     });
