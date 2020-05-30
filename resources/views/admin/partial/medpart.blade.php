4f44b13d27ad07e81089cd0780ed5ecd778e354f
dimaspndw123 <65652924+dimaspndw123@users.noreply.github.com>
2020-05-30 19:52:03 +0700

    view sponsor

---
+++ resources/views/admin/partial/medpart.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sponsorship</title>
+</head>
+
+<body>
+    Tambah Media Partner
+    <form method="POST" action="">
+        <li>
+            <input type="file" name="sponsor">
+        </li>
+        <button type="submit">Tambah Sponsor</button>
+    </form>
+</body>
+
+</html>
