1bd59f87e8f2bad8f81e487e8f4ccbe932215a8e
whitefay <nursabilly.kun@gmail.com>
2020-06-01 22:35:03 +0700

    Edit favicon, tambah Tema di comingsoon

--- resources/views/comingsoon.blade.php
+++ resources/views/comingsoon.blade.php
@@ -5,47 +5,9 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <!--===============================================================================================-->
-    <link rel="apple-touch-icon" sizes="57x57" href="{{ asset('assets/comingsoon/images/icons/apple-icon-57x57.png') }}" />
-    <link rel="apple-touch-icon" sizes="60x60" href="{{ asset('assets/comingsoon/images/icons/apple-icon-60x60.png') }}" />
-    <link rel="apple-touch-icon" sizes="72x72" href="{{ asset('assets/comingsoon/images/icons/apple-icon-72x72.png') }}" />
-    <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('assets/comingsoon/images/icons/apple-icon-76x76.png') }}" />
-    <link
-      rel="apple-touch-icon"
-      sizes="114x114"
-      href="images/icons/apple-icon-114x114.png"
-    />
-    <link
-      rel="apple-touch-icon"
-      sizes="120x120"
-      href="images/icons/apple-icon-120x120.png"
-    />
-    <link
-      rel="apple-touch-icon"
-      sizes="144x144"
-      href="images/icons/apple-icon-144x144.png"
-    />
-    <link
-      rel="apple-touch-icon"
-      sizes="152x152"
-      href="images/icons/apple-icon-152x152.png"
-    />
-    <link
-      rel="apple-touch-icon"
-      sizes="180x180"
-      href="images/icons/apple-icon-180x180.png"
-    />
-    <link
-      rel="icon"
-      type="image/png"
-      sizes="192x192"
-      href="images/icons/android-icon-192x192.png"
-    />
     <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('assets/comingsoon/images/icons/favicon-32x32.png') }}" />
     <link rel="icon" type="image/png" sizes="96x96" href="{{ asset('assets/comingsoon/images/icons/favicon-96x96.png') }}" />
     <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('assets/comingsoon/images/icons/favicon-16x16.png') }}" />
-    {{-- <link rel="manifest" href="{{ asset('assets/comingsoon/manifest.json') }}" /> --}}
-    <meta name="msapplication-TileColor" content="#ffffff" />
-    <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
     <meta name="theme-color" content="#ffffff" />
     <!--===============================================================================================-->
     <link
@@ -93,7 +55,7 @@
         <div class="wrappic1 mx-auto text-center">
           <a href="#"
             ><img
-              src="assets/comingsoon/images/icons/logo2020.png"
+              src="{{ asset('assets/comingsoon/images/icons/logo2020.png') }}"
               alt="Anforcom 2020"
               title="Logo Anforcom 2020"
           /></a>
@@ -101,7 +63,7 @@
       </div>
 
       <!--  -->
-      <div class="flex-col-c-m p-l-15 p-r-15 p-t-50 p-b-120">
+      <div class="flex-col-c-m p-l-15 p-r-15 p-t-50 p-b">
         <h3 class="l1-txt1 txt-center p-b-40 respon1">
           Coming Soon
         </h3>
@@ -127,8 +89,9 @@
             <span class="s1-txt1 where1 mx-auto">Seconds</span>
           </div>
         </div>
-        <h2 id="dateDisplay" class="l1-txt1 txt-center p-b-40" style="font-size: 36px; color: white">
-        </h3>
+        <h2 id="dateDisplay" class="l1-txt1 txt-center p-b-40" style="font-size: 24px; color: white">
+        </h2>
+        <h2 class="l1-txt2 txt-center p-b-40" style="font-size: 36px; color: white">Reconnecting Society with<br>Advanced Technology</h2>
       </div>
 
       <!--  -->
