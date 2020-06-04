a7a454b6886277c3d2ec60699ff8d2a55e941147
Muhammad Keenan <mkf450@gmail.com>
2020-06-04 15:15:55 +0700

    Update infolomba

--- frontend/infolomba/assets/js/main.js
+++ frontend/infolomba/assets/js/main.js
@@ -15,10 +15,7 @@ $(function() {
         var scroll = $(window).scrollTop();
         if (scroll < 20) {
             $(".navbar-area").removeClass("sticky");
-            $(".navbar-area img").attr("src", "assets/images/logoav.png");
-        } else {
-            $(".navbar-area").addClass("sticky");
-            $(".navbar-area img").attr("src", "assets/images/logoav.png");
+            $(".navbar-area img").attr("src", "assets/images/logobaru.png");
         }
     });
 
