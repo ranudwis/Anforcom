42802cf4a70cf900724df6e5775817b06c21a862
Ranu Dwi Setiadi <ranudwis@gmail.com>
2020-06-06 12:04:41 +0700

    Gunakan fungsi asset untuk scripts

--- resources/views/partial/scripts.blade.php
+++ resources/views/partial/scripts.blade.php
@@ -1,27 +1,27 @@
 <!-- Jquery js-->
-<script src="assets/js/vendor/jquery-1.12.4.min.js"></script>
-<script src="assets/js/vendor/modernizr-3.7.1.min.js"></script>
+<script src="{{ asset('assets/js/vendor/jquery-1.12.4.min.js') }}"></script>
+<script src="{{ asset('assets/js/vendor/modernizr-3.7.1.min.js') }}"></script>
 
 <!-- Bootstrap js -->
-<script src="assets/js/popper.min.js"></script>
-<script src="assets/js/bootstrap.min.js"></script>
+<script src="{{ asset('assets/js/popper.min.js') }}"></script>
+<script src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
 
 <!-- Slick js -->
-<script src="assets/js/slick.min.js"></script>
+<script src="{{ asset('assets/js/slick.min.js') }}"></script>
 
 <!-- Magnific Popup js -->
-<script src="assets/js/jquery.magnific-popup.min.js"></script>
+<script src="{{ asset('assets/js/jquery.magnific-popup.min.js') }}"></script>
 
 <!-- Ajax Contact js -->
-<script src="assets/js/ajax-contact.js"></script>
+<script src="{{ asset('assets/js/ajax-contact.js') }}"></script>
 
 <!-- Isotope js -->
-<script src="assets/js/imagesloaded.pkgd.min.js"></script>
-<script src="assets/js/isotope.pkgd.min.js"></script>
+<script src="{{ asset('assets/js/imagesloaded.pkgd.min.js') }}"></script>
+<script src="{{ asset('assets/js/isotope.pkgd.min.js') }}"></script>
 
 <!-- Scrolling Nav js -->
-<script src="assets/js/jquery.easing.min.js"></script>
-<script src="assets/js/scrolling-nav.js"></script>
+<script src="{{ asset('assets/js/jquery.easing.min.js') }}"></script>
+<script src="{{ asset('assets/js/scrolling-nav.js') }}"></script>
 
 <!-- Main js -->
-<script src="assets/js/main.js"></script>
+<script src="{{ asset('assets/js/main.js') }}"></script>
