170dc3e03cb5945c4e06f94817697ba04e638ece
Nur Sabilly <nursabilly@localhost.localdomain>
2020-06-07 20:54:11 +0700

    Tambah halaman dashboard user

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,53 +1,68 @@
 @extends('dashboard.partial.main')
 
 @section('content')
-    <main class="container-fluid">
+<article class="col-md-9 position-relative">
     <div class="row">
-        <div class="col-md-12 d-flex justify-content-center">
-            <h2 class="pt-5 pb-5">Dashboard</h2>
+        <div class="col-md">
+            <h6 class="pt-5 pb-3 ml-5" style="color:rgba(0,0,0,0.5)">Dashboard (breadcumbs)</h6>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md">
+            <h4 class="ml-5">Selamat Datang, </h4>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md">
+            <h3 class="ml-5 font-weight-bold">Nama Pengguna</h3>
         </div>
     </div>
     @if ($registrations->isEmpty())
         <div class="row">
-            <div class="col-md-12 d-flex justify-content-start">
+            <div class="col-md">
                 <h3>Kamu belum terdaftar pada lomba atau event apapun</h3>
             </div>
         </div>
     @else
     <div class="row">
-        <div class="col-md-12 d-flex justify-content-center">
-            <h3>Kamu terdaftar pada : </h3>
+        <div class="col-md">
+            <h6 class="ml-5 mt-3">Event yang kamu ikuti: </h6>
         </div>
     </div>
+    @foreach ($registrations as $registration)
     <div class="row">
-        <div class="container">
-            @foreach ($registrations as $registration)
-            <div class="card" style="margin-top: 0.5rem">
+        <div class="col-md">
+            <div class="card rounded shadow-sm mx-5 my-2">
                 <div class="card-body">
                     <h5 class="card-title">
-                        <a href="#" alt="{{$registration->event->name}}" title="{{$registration->event->name}}">{{$registration->event->name}}</a>
+                        <a class="font-weight-bold"href="#" style="color:black;text-decoration:none;"alt="{{$registration->event->name}}" title="{{$registration->event->name}}">{{$registration->event->name}}</a>
                     </h5>
                     @if ($registration->isInactive())
-                        <a href="#" class="btn btn-warning"><i class="fa fa-frown"></i>&nbsp;Belum Bayar</a>
+                        <span>Status Pembayaran : <span class="text-warning">Belum Membayar</span></span>
                     @elseif ($registration->isPaid())
-                        <a href="#" class="btn btn-info"><i class="fa fa-clock"></i>&nbsp;Menunggu Konfirmasi</a>
+                        <span>Status Pembayaran : <span class="text-info">Menunggu Konfirmasi</span></span>
                     @elseif ($registration->isRejected())
-                        <a href="#" class="btn btn-danger"><i class="fa fa-times"></i>&nbsp;Pembayaran Gagal</a>
+                    <span>Status Pembayaran : <span class="text-danger">Gagal</span></span>
                     @elseif ($registration->isActive())
-                        <a href="#" class="btn btn-danger"><i class="fa fa-check"></i>&nbsp;Terdaftar</a>
+                    <span>Status Pembayaran : <span class="text-success">Aktif</span></span>
                     @endif
                 </div>
             </div>
-            @endforeach
         </div>
     </div>
-    @endif
+    @endforeach
     <div class="row">
-        <div class="container">
-            <div class="col-md-12">
-                <a class="btn btn-primary mt-2 mb-2 flex-d justify-content-center" href="{{ route('enroll.index') }}">Daftar event atau lomba lain</a>
+        <div class="col-md mb-5">
+            <div class="card rounded shadow-sm mx-5 my-2">
+                <div class="card-body">
+                    <h5 class="card-title text-center">
+                        <a href="#" alt="Daftar Event Lain" title="Daftar Event Lain" style="text-decoration: none; color: black"><i class="fa fa-plus"></i></a>
+                    </h5>
+                </div>
             </div>
         </div>
     </div>
-    </main>
+    </div>
+    @endif
+</article>
 @endsection
--- resources/views/dashboard/partial/main.blade.php
+++ resources/views/dashboard/partial/main.blade.php
@@ -10,41 +10,35 @@
     </head>
     <body>
       {{-- navbar --}}
-        <nav class="navbar navbar-expand-lg navbar-light bg-light" style="border-bottom: 1px solid rgba(0,0,0,.125)">
-        <a class="navbar-brand" href="#">Anforcom 2020</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-              <ul class="navbar-nav ml-auto">
-                <li class="nav-item active">
-                  <a class="nav-link" href="{{ route('dashboard.index') }}">Dashboard<span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="{{ route('dashboard.payment') }}"">Pembayaran</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="{{ route('logout') }}">Logout</a>
-                </li>
-              </ul>
-            </div>
+      <div class="container-fluid">
+        <div class="row">
+          <nav class="navbar navbar-light bg-white shadow-sm rounded col-md mt-1 mb-1 ml-1 mr-1">
+            <a class="navbar-brand py-3" href="#">
+              <img src="{{asset('assets/images/logopanjang.png')}}" width="20%" alt loading="lazy">
+            </a>
           </nav>
-          {{-- End Navbar --}}
+        </div>
+      </div>
           @if (session('status'))
           <div>
               {{session('status')}}
           </div>
           @endif
-        {{-- Content --}}
-        @yield('content')
-        {{-- Footer --}}
-          <footer class="py-4 bg-dark text-white-50" id="sticky-footer">
-            <div class="container">
-              <p><a href="http://anforcom.com" alt="Anforcom 2020" title="Anforcom 2020">Anforcom 2020</a> - Copyright 2020</p>
+          <main class="container-fluid">
+            <div class="row">
+                <aside class="col-md-3 bg-light shadow-sm position-relative">
+                    <div class="card my-5 mx-auto" style="width: 16rem;">
+                        <ul class="list-group list-group-flush">
+                          <li class="list-group-item"><a style="text-decoration:none;color:black;" href="#" alt="Dashboard" title="Dashboard"><i class="fa fa-home"></i>&#9;Dashboard</a></li>
+                          <li class="list-group-item"><a style="text-decoration:none;color:black;" href="#" alt="Timeline" title="Timeline"><i class="fa fa-calendar-alt"></i>&#9;Timeline</a></li>
+                          <li class="list-group-item"><a style="text-decoration:none;color:black;" href="#" alt="Payment" title="Payment"><i class="fa fa-credit-card"></i>&#9;Payment</a></li>
+                        </ul>
+                      </div>
+                </aside>
+                {{-- Content --}}
+              @yield('content')
             </div>
-          </footer>
-
-
+        </main>
         <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
