95b98e69e8a4f9909f4d9a0fd63ad147ad9e0b08
Ranu Dwi Setiadi <ranudwis@gmail.com>
2020-06-11 11:57:19 +0700

    Pisahkan template partial dashboard

---
+++ resources/views/dashboard/partial/asidenav.blade.php
@@ -0,0 +1,26 @@
+<aside class="col-md-3 bg-light shadow-sm position-relative">
+    <div class="card my-5 mx-auto" style="width: 16rem;">
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item">
+                <a style="text-decoration:none;color:black;" href="#" alt="Dashboard" title="Dashboard">
+                    <i class="fa fa-home"></i>
+                    &emsp;Dashboard
+                </a>
+            </li>
+
+            <li class="list-group-item">
+                <a style="text-decoration:none;color:black;" href="#" alt="Timeline" title="Timeline">
+                    <i class="fa fa-calendar-alt"></i>
+                    &emsp;Timeline
+                </a>
+            </li>
+
+            <li class="list-group-item">
+                <a style="text-decoration:none;color:black;" href="#" alt="Payment" title="Payment">
+                    <i class="fa fa-credit-card"></i>
+                    &emsp;Payment
+                </a>
+            </li>
+        </ul>
+    </div>
+</aside>
---
+++ resources/views/dashboard/partial/navbar.blade.php
@@ -0,0 +1,9 @@
+<div class="container-fluid">
+    <div class="row">
+        <nav class="navbar navbar-light bg-white shadow-sm rounded col-md mt-1 mb-1 ml-1 mr-1">
+            <a class="navbar-brand py-3" href="#">
+                <img src="{{asset('assets/images/logolong.png')}}" width="20%" alt loading="lazy">
+            </a>
+        </nav>
+    </div>
+</div>
