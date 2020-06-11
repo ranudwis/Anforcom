92eeb4b517c2079dbbe0e6a9b49f9066277130f2
HusniFadhilah <husnfadhilah62@gmail.com>
2020-06-11 19:49:09 +0700

    Lanjut memindahkan form ke resources/views

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,24 +1,146 @@
 <!DOCTYPE html>
 <html lang="id" dir="ltr">
-    <head>
-        <meta charset="utf-8">
-        <title>Daftar</title>
-    </head>
-    <body>
-        daftar anforcom
-        @if ($errors->any())
-            @foreach ($errors->all() as $error)
-                {{ $error }}
-            @endforeach
-        @endif
-        <form method="post" action="{{ $formAction }}">
-            @csrf
-            <input type="text" name="name" placeholder="nama" /><br />
-            <input type="text" name="email" placeholder="email" /><br />
-            <input type="text" name="password" placeholder="password" /><br />
-            <input type="text" name="password_confirmation" placeholder="ulangi password" /><br />
-            <input type="text" name="contact" placeholder="Line / WA" /><br />
-            <button type="submit">Daftar</button>
-        </form>
-    </body>
+
+<head>
+    <title>Register</title>
+    <!--  meta tags -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/form/vendor/bootstrap/css/bootstrap.min.css') }}">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/form/fonts/font-awesome-4.7.0/css/font-awesome.min.css') }}">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/form/css/util.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/form/css/main.css') }}">
+    <!--===============================================================================================-->
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('assets/form/images/icons/favicon-32x32.png') }}" />
+    <link rel="icon" type="image/png" sizes="96x96" href="{{ asset('assets/form/images/icons/favicon-96x96.png') }}" />
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('assets/form/images/icons/favicon-16x16.png') }}" />
+    <!--===============================================================================================-->
+</head>
+
+<body>
+    <!-- daftar anforcom
+    @if ($errors->any())
+    @foreach ($errors->all() as $error)
+    {{ $error }}
+    @endforeach
+    @endif
+    <form method="post" action="{{ $formAction }}">
+        @csrf
+        <input type="text" name="name" placeholder="nama" /><br />
+        <input type="text" name="email" placeholder="email" /><br />
+        <input type="text" name="password" placeholder="password" /><br />
+        <input type="text" name="password_confirmation" placeholder="ulangi password" /><br />
+        <input type="text" name="contact" placeholder="Line / WA" /><br />
+        <button type="submit">Daftar</button>
+    </form> -->
+
+    <div class="limiter">
+        <div class="login100-more" style="background-image: url({{ asset('assets/form/images/illustration.png') }});">
+        </div>
+        <div class="container-login100">
+            <div class="wrap-login100 justify-content-center">
+                <form class="login100-form validate-form pt-5" method="post" action="{{ $formAction }}">
+                    <div class="login100-logo mx-auto ml-lg-0 pt-lg-5">
+                        <img src="{{ asset('assets/form/images/LOGO2.png') }}" class="img-fluid">
+                    </div>
+                    <span class="login100-form-title mt-3 p-b-34">
+                        <h4 class="font-weight-bold text-dark text-left text-center text-lg-left">Buat Akun Baru</h4>
+                    </span>
+                    @if ($errors->any())
+                    @foreach ($errors->all() as $error)
+                    {{ $error }}
+                    @endforeach
+                    @endif
+                    <div class="alert alert-danger btn-block" role="alert">
+                        This is an example danger alert for validate form
+                    </div>
+                    @csrf
+                    <div class="form-group-full">
+                        <label for="username">Username</label>
+                        <div class="wrap-input100 rs1-wrap-input100 validate-input m-b-20" data-validate="Type user name">
+                            <input id="username" class="input100" type="text" name="name" placeholder="anforcom">
+                            <span class="focus-input100"></span>
+                        </div>
+                    </div>
+                    <div class="form-group-full">
+                        <label for="email">Email</label>
+                        <div class="wrap-input100 rs1-wrap-input100 validate-input m-b-20" data-validate="Type email">
+                            <input id="email" class="input100" type="email" name="email" placeholder="anforcom2020@gmail.com">
+                            <span class="focus-input100"></span>
+                        </div>
+                    </div>
+                    <div class="form-group-half pr-2">
+                        <label for="first-name">Password</label>
+                        <div class="wrap-input100 rs1-wrap-input100 validate-input m-b-20" data-validate="Type password">
+                            <input class="input100" type="password" name="password" placeholder="**********">
+                            <span class="focus-input100"></span>
+                        </div>
+                    </div>
+                    <div class="form-group-half pl-2">
+                        <label for="first-name">Konfirmasi Password</label>
+                        <div class="wrap-input100 rs1-wrap-input100 validate-input m-b-20" data-validate="Type password">
+                            <input class="input100" type="password" name="password_confirmation" placeholder="**********">
+                            <span class="focus-input100"></span>
+                        </div>
+                    </div>
+                    <div class="form-group-full">
+                        <label for="contact">Contact</label>
+                        <div class="wrap-input100 rs1-wrap-input100 validate-input m-b-20" data-validate="Type contact">
+                            <input class="input100" type="text" name="contact" placeholder="(Whatsapp/Line)">
+                            <span class="focus-input100"></span>
+                        </div>
+                    </div>
+                    <div class="container pl-0 pr-0 utilities mb-4" style="border-radius: 50%">
+                        <span class="float-left">
+                            <div class="round">
+                                <input class="mt-0 mr-2" type="checkbox" id="checkbox" />
+                                <label class="m-t-2" for="checkbox"></label> Ingat Saya
+                            </div>
+                        </span>
+                        <a href="" class="float-right">Lupa Password </a>
+                    </div>
+                    <div class="container-login100-form-btn p-b-50">
+                        <button class="login100-form-btn">
+                            Register
+                        </button>
+                    </div>
+
+                    <div class="w-full text-center pb-lg-2">
+                        <a href="#" class="">
+                            Sudah punya akun? <a href="{{ route('login') }}" data-url="{{ route('login') }}" class="text-warning">Masuk di sini!</a>
+                        </a>
+                    </div>
+                </form>
+
+
+            </div>
+        </div>
+    </div>
+
+    <!--===============================================================================================-->
+    <script src="{{ asset('assets/form/vendor/jquery/jquery-3.2.1.min.js') }}"></script>
+    <!--===============================================================================================-->
+    <script src="{{ asset('assets/form/vendor/bootstrap/js/popper.js') }}"></script>
+    <script src="{{ asset('assets/form/vendor/bootstrap/js/bootstrap.min.js') }}"></script>
+    <!--===============================================================================================-->
+    <!-- <script src="{{ asset('assets/form/assets/js/main.js') }}"></script> -->
+
+    <!-- <script>
+        $(document).ready(function() {
+            $("a").click(function(event) {
+                var $this = $(this),
+                    url = $this.data("url");
+
+                $(document.body).load(url);
+                event.preventDefault();
+            });
+        });
+    </script> -->
+
+</body>
+
 </html>
--- resources/views/enroll/show.blade.php
+++ resources/views/enroll/show.blade.php
@@ -1,21 +1,35 @@
 <!DOCTYPE html>
 <html lang="id">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Form Pendaftaran {{ $event->name }}</title>
+    <!--  meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!--===============================================================================================-->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/form/css/style.css') }}">
+    <!--===============================================================================================-->
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('assets/form/images/icons/favicon-32x32.png') }}" />
+    <link rel="icon" type="image/png" sizes="96x96" href="{{ asset('assets/form/images/icons/favicon-96x96.png') }}" />
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('assets/form/images/icons/favicon-16x16.png') }}" />
+    <!--===============================================================================================-->
 </head>
 
 <body>
-    <h2>Form Pendaftaran Anforcom</h2>
+    <!-- <h2>Form Pendaftaran Anforcom</h2>
 
     <form method="POST" action="{{ route('enroll.create', ['event' => $event->slug]) }}" enctype="multipart/form-data">
         @csrf
 
         @if ($errors->any())
-            @foreach ($errors->all() as $error)
-                {{ $error }}
-            @endforeach
+        @foreach ($errors->all() as $error)
+        {{ $error }}
+        @endforeach
         @endif
         <h3> {{ $event->name }}</h3>
         <div class="form-group">
@@ -89,8 +103,233 @@
         </div>
 
         <button type="submit">Daftar</button>
-    </form>
+    </form> -->
+    <section id="register">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 col-lg-8 text-center mx-auto pt-5" id="judulregis">
+                    <img src="assets/images/LOGO2.png" class="img-fluid mb-2" style="max-height: 30px;">
+                    <h2 class="form-title">Form Pendaftaran {{ $event->name }}</h2>
+                </div>
+            </div>
+            <div class="alert alert-danger btn-block mt-3" role="alert">
+                This is an example danger alert for validate form
+            </div>
+            @csrf
 
+            @if ($errors->any())
+            @foreach ($errors->all() as $error)
+            {{ $error }}
+            @endforeach
+            @endif
+            <div class="mt-4" id="form-regis">
+                <form class="validate-form" novalidate>
+                    <div class="row pt-3">
+                        <div class="col-md-6">
+                            <div class="card pb-0 pt-2 mb-5">
+                                <div class="card-body">
+                                    <div class="card-title">Informasi Tim</div>
+                                    <div class="form-group">
+                                        <div class="input-group">
+                                            <div class="input-group-prepend">
+                                                <span class="input-group-text">
+                                                    <i class="fa fa-university"></i>
+                                                </span>
+                                            </div>
+                                            <input class="form-control" id="university" type="text" name="university" value="{{old('university')}}" required>
+                                            <label for="university" class="form-control-placeholder">Perguruan Tinggi
+                                                *</label>
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <div class="input-group">
+                                            <div class="input-group-prepend">
+                                                <span class="input-group-text">
+                                                    <i class="fas fa-clipboard-list"></i>
+                                                </span>
+                                            </div>
+                                            <input class="form-control" id="team_name" type="text" name="team_name" value="{{old('team_name')}}" required>
+                                            <label for="team_name" class="form-control-placeholder">Nama Tim Anda
+                                                *</label>
+                                            <!-- <span id='bantuan_nama_tim' style='display: none; color: red'>Team sudah
+                                                Terdaftar</span> -->
+                                        </div>
+                                    </div>
+
+                                    <div class="card-title mt-5">Ketua Tim</div>
+                                    <div class="form-group">
+                                        <div class="input-group">
+                                            <div class="input-group-prepend">
+                                                <span class="input-group-text">
+                                                    <i class="fas fa-user"></i>
+                                                </span>
+                                            </div>
+                                            <input class="form-control" id="leader_name" type="text" name="leader_name" value="{{old('leader_name')}}" required>
+                                            <label for="leader_name" class="form-control-placeholder">Nama Ketua
+                                                *</label>
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <div class="input-group">
+                                            <div class="input-group-prepend">
+                                                <span class="input-group-text">
+                                                    <i class="fa fa-id-card"></i>
+                                                </span>
+                                            </div>
+                                            <input class="form-control" id="leader_nim" type="text" name="leader_nim" value="{{old('leader_nim')}}" required>
+                                            <label for="leader_nim" class="form-control-placeholder">Nomor Mahasiswa
+                                                Ketua (NIM) *</label>
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="">Scan Kartu Tanda Mahasiswa Ketua *
+                                            <small>.png/.jpg</small></label>
+                                        <input class="form-control-file" type="file" name="leader_ktm" required>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-md-6">
+                            <div class="card pt-2 mb-2">
+                                <div class="card-body">
+                                    <div class="card-title">Anggota Tim</div>
+                                    <div class="form-group">
+                                        <div class="input-group">
+                                            <div class="input-group-prepend">
+                                                <span class="input-group-text">
+                                                    <i class="fa fa-user-plus"></i>
+                                                </span>
+                                            </div>
+                                            <input class="form-control" id="nama_anggota1" type="text" name="members[0][name]" value="{{ old('members[0][name]') }}" required>
+                                            <label for="nama_anggota1" class="form-control-placeholder">Nama Anggota
+                                                1</label>
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <div class="input-group">
+                                            <div class="input-group-prepend">
+                                                <span class="input-group-text">
+                                                    <i class="fa fa-id-card"></i>
+                                                </span>
+                                            </div>
+                                            <input class="form-control" id="nim_anggota1" type="text" name="members[0][nim]" value="" required>
+                                            <label for="nim_anggota1" class="form-control-placeholder">Nomor Mahasiswa
+                                                Anggota 1 (NIM)</label>
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <div class="input-group">
+                                            <div class="input-group-prepend">
+                                                <span class="input-group-text">
+                                                    <i class="fa fa-at"></i>
+                                                </span>
+                                            </div>
+                                            <input class="form-control" id="email_anggota1" type="text" name="members[0][email]" value="" required>
+                                            <label for="email_anggota1" class="form-control-placeholder">Email
+                                                Anggota 1</label>
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <div class="input-group">
+                                            <div class="input-group-prepend">
+                                                <span class="input-group-text">
+                                                    <i class="fa fa-phone"></i>
+                                                </span>
+                                            </div>
+                                            <input class="form-control" id="kontak_anggota1" type="text" name="members[0][contact]" value="" required>
+                                            <label for="kontak_anggota1" class="form-control-placeholder">Line/Whatsapp
+                                                Anggota 1</label>
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="">Scan Kartu Tanda Mahasiswa Anggota 1
+                                            <small>.png/.jpg</small></label>
+                                        <input class="form-control-file" type="file" name="members[0][ktm]">
+                                    </div>
+                                    <div class="form-group">
+                                        <div class="input-group">
+                                            <div class="input-group-prepend">
+                                                <span class="input-group-text">
+                                                    <i class="fa fa-user-plus"></i>
+                                                </span>
+                                            </div>
+                                            <input class="form-control" id="nama_anggota2" type="text" name="members[1][name]" value="" required>
+                                            <label for="nama_anggota2" class="form-control-placeholder">Nama Anggota
+                                                2</label>
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <div class="input-group">
+                                            <div class="input-group-prepend">
+                                                <span class="input-group-text">
+                                                    <i class="fa fa-id-card"></i>
+                                                </span>
+                                            </div>
+                                            <input class="form-control" id="nim_anggota2" type="text" name="members[1][nim]" value="" required>
+                                            <label for="nim_anggota2" class="form-control-placeholder">Nomor Mahasiswa
+                                                Anggota 2</label>
+                                            <div class="invalid-feedback">
+                                                Looks good!
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <div class="input-group">
+                                            <div class="input-group-prepend">
+                                                <span class="input-group-text">
+                                                    <i class="fa fa-at"></i>
+                                                </span>
+                                            </div>
+                                            <input class="form-control" id="email_anggota2" type="text" name="members[1][email]" value="" required>
+                                            <label for="email_anggota2" class="form-control-placeholder">Email
+                                                Anggota 2</label>
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <div class="input-group">
+                                            <div class="input-group-prepend">
+                                                <span class="input-group-text">
+                                                    <i class="fa fa-phone"></i>
+                                                </span>
+                                            </div>
+                                            <input class="form-control" id="kontak_anggota2" type="text" name="members[1][contact]" value="" required>
+                                            <label for="kontak_anggota2" class="form-control-placeholder">Line/Whatsapp
+                                                Anggota 2 (NIM)</label>
+                                            <div class="invalid-feedback">
+                                                Looks good!
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="form-group upload">
+                                        <label for="">Scan Kartu Tanda Mahasiswa Anggota 2
+                                            <small>.png/.jpg</small></label>
+                                        <input class="form-control-file" type="file" name="members[1][ktm]">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6 mb-5" id="button_keterangan">
+                            <p class="mb-1"><small>*) Wajib untuk diisi</small></p>
+                            <button class="btn btn-block d-flex justify-content-center mx-auto" type="submit">DAFTAR!</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </section>
+    <!-- - - - - -end- - - - -  -->
+
+    <!-- Optional JavaScript -->
+    <!--===============================================================================================-->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+    </script>
+    <!--===============================================================================================-->
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+    </script>
+    <!--===============================================================================================-->
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous">
+    </script>
 </body>
 
 </html>
