c7d257f04e0c22d60368330cefd3ce921e2a3a48
Ranu Dwi Setiadi <ranudwis@gmail.com>
2020-06-11 12:46:51 +0700

    Tambahkan link pada asidenav dashboard

--- resources/views/dashboard/partial/asidenav.blade.php
+++ resources/views/dashboard/partial/asidenav.blade.php
@@ -2,23 +2,30 @@
     <div class="card my-5 mx-auto" style="width: 80%;">
         <ul class="list-group list-group-flush">
             <li class="list-group-item">
-                <a style="text-decoration:none;color:black;" href="#" alt="Dashboard" title="Dashboard">
-                    <i class="fa fa-home"></i>
+                <a style="text-decoration:none;color:black;" href="{{ route('dashboard.index') }}" alt="Dashboard" title="Dashboard">
+                    <i class="fa fa-fw fa-house-user"></i>
                     &emsp;Dashboard
                 </a>
             </li>
 
             <li class="list-group-item">
                 <a style="text-decoration:none;color:black;" href="#" alt="Timeline" title="Timeline">
-                    <i class="fa fa-calendar-alt"></i>
+                    <i class="fa fa-fw fa-calendar-alt"></i>
                     &emsp;Timeline
                 </a>
             </li>
 
             <li class="list-group-item">
-                <a style="text-decoration:none;color:black;" href="#" alt="Payment" title="Payment">
-                    <i class="fa fa-credit-card"></i>
-                    &emsp;Payment
+                <a style="text-decoration:none;color:black;" href="{{ route('home') }}" alt="Anforcom home" title="Anforcom Home">
+                    <i class="fa fa-fw fa-home"></i>
+                    &emsp;Anforcom Home
+                </a>
+            </li>
+
+            <li class="list-group-item">
+                <a style="text-decoration:none;color:black;" href="{{ route('logout') }}" alt="Logout" title="Logout">
+                    <i class="fa fa-fw fa-sign-out-alt"></i>
+                    &emsp;Logout
                 </a>
             </li>
         </ul>
