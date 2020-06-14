c4290b97de93ca63a0ab09e526b846e72911f8b4
Muhammad Keenan <mkf450@gmail.com>
2020-06-14 14:48:52 +0700

    nambah homepage

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -2,6 +2,7 @@
     <html lang="id" dir="ltr">
 
     <head>
+        @include('partial.headtag')
         <meta charset="utf-8">
         <title>{{ env('APP_NAME', 'Anforcom') }}</title>
     </head>
@@ -10,6 +11,190 @@
         <a href="{{ route('competition.index') }}">List Lomba</a>
         <a href="{{ route('login') }}">Masuk</a>
         <a href="{{ route('register') }}">Daftar</a>
+
+        <!-- HOME START -->
+        <section id="home" class="header">
+          <div class="header-content">
+              <div class="container">
+                  <div class="row">
+                      <div class="col-lg-6">
+                          <div class="text-container">
+                              <h1>Anforcom 2020</h1>
+                              <p class="text">Annual Informatics Competition (Anforcom) 2020 merupakan kompetisi skala nasional yang diselenggarakan oleh Himpunan Mahasiswa Informatika Universitas Diponegoro.</p>
+                              <ul class="slider-btn rounded-buttons">
+                                <li><a class="main-btn rounded-one" href="#">Daftar</a></li>
+                              </ul>
+                          </div>
+                      </div> <!-- end of col -->
+                      <div class="col-lg-6">
+                          <div class="image-container">
+                              <img class="img-fluid" src="#" alt="blom ada">
+                          </div> <!-- end of image-container -->
+                      </div> <!-- end of col -->
+                  </div> <!-- end of row -->
+              </div> <!-- end of container -->
+          </div> <!-- end of header-content -->
+        </section>
+        <!-- HOME ENDS -->
+
+        <!-- ABOUT START -->
+        <section>
+          <div id="about" class="about">
+            <div class="container">
+              <div class="row">
+                <div class="col-lg-12">
+                  <div class="section-title text-center pb-10">
+                    <h3 class="title">Apa itu Anforcom?</h3>
+                  </div>
+                </div>
+                <div class="col-xs-12 col-md-8">
+                  <p class="text">Anforcom 2020 kembali hadir dengan berfokus pada kemampuan peserta dalam software development dan data mining.</p>
+                  <p class="text">Anforcom pada tahun ini mengusung tema yaitu "Reconnecting Society with Advanced Technology" dengan harapan mahasiswa Indonesia dapat membuat aplikasi se-kreatif dan se-inovatif mungkin yang dapat menciptakan generasi cemerlang untuk Indonesia di masa depan.</p>
+                </div>
+                <div class="col-xs-6 col-md-4">
+                  <div class="image-container">
+                    <img class="img-fluid" src="assets/images/logo.png" alt="logoav">
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <!-- ABOUT END -->
+
+        <!-- EVENT START -->
+        <section id="event" class="event">
+          <div class="container">
+            <div class="row">
+              <div class="col-lg-12">
+                <div class="section-title text-center pb-10">
+                  <h3 class="title">Kompetisi dan acara</h3>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <!-- EVENT END -->
+
+        <!-- TEASER START -->
+        <section id="teaser">
+          <div class="basic-3">
+            <div class="container">
+              <div class="row">
+                <div class="col-lg-12">
+                  <h2 class="title">Teaser Anforcom 2020</h2>
+                </div> <!-- end of col -->
+              </div> <!-- end of row -->
+              <div class="row">
+                <div class="col-lg-12">
+                  <div class="image-container">
+                    <div class="video-wrapper">
+                      <a class="popup-youtube" href="https://www.youtube.com/watch?v=YYNhPgzVDnM" data-effect="fadeIn">
+                        <img class="img-fluid" src="assets/images/video.jpg" alt="alternative">
+                        <span class="video-play-button">
+                          <span></span>
+                        </span>
+                      </a>
+                    </div> <!-- end of video-wrapper -->
+                  </div> <!-- end of image-container -->
+                </div> <!-- end of col -->
+              </div> <!-- end of row -->
+            </div> <!-- end of container -->
+          </div> <!-- end of basic-3 -->
+        </section>
+        <!-- TEASER END -->
+
+        <!-- SPONSOR START -->
+        <section id="sponsor" class="sponsor">
+          <div class="brand-area pt-90">
+            <div class="container">
+              <div class="row">
+                <div class="col-lg-12">
+                  <div class="section-title text-center pb-10">
+                    <h3 class="title">Sponsored by</h3>
+                  </div>
+                </div>
+                <div class="col-lg-12">
+                  <div class="brand-logo d-flex align-items-center justify-content-center justify-content-md-between">
+                    <div class="single-logo mt-30 wow fadeIn">
+                      <img src="assets/images/sponsor/Google.png" alt="brand">
+                    </div>
+                    <div class="single-logo mt-30 wow fadeIn">
+                      <img src="assets/images/sponsor/Facebook.png" alt="brand">
+                    </div>
+                    <div class="single-logo mt-30 wow fadeIn">
+                      <img src="assets/images/sponsor/Tesla.png" alt="brand">
+                    </div>
+                    <div class="single-logo mt-30 wow fadeIn">
+                      <img src="assets/images/sponsor/Gojek.png" alt="brand">
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <!-- SPONSOR PARTNER END -->
+
+        <!-- MEDIA PARTNER END -->
+        <section id="medpar" class="medpar">
+          <div class="brand-area pt-90">
+            <div class="container">
+              <div class="row">
+                <div class="col-lg-12">
+                  <div class="section-title text-center pb-10">
+                    <h3 class="title">Media Partner</h3>
+                  </div>
+                </div>
+                <div class="col-lg-12">
+                  <div class="brand-logo d-flex align-items-center justify-content-center justify-content-md-between">
+                    <div class="single-logo mt-30 wow fadeIn">
+                      <img src="assets/images/sponsor/NYT.png" alt="brand">
+                    </div>
+                    <div class="single-logo mt-30 wow fadeIn">
+                      <img src="assets/images/sponsor/BBC.png" alt="brand">
+                    </div>
+                    <div class="single-logo mt-30 wow fadeIn">
+                      <img src="assets/images/sponsor/CNN.png" alt="brand">
+                    </div>
+                    <div class="single-logo mt-30 wow fadeIn">
+                      <img src="assets/images/sponsor/Bloomberg.png" alt="brand">
+                    </div>
+                    <div class="single-logo mt-30 wow fadeIn">
+                      <img src="assets/images/sponsor/JP.png" alt="brand">
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <!-- MEDIA PARTNER END -->
+
+        <!-- FOOTER START -->
+        <footer id="footer" class="footer-area">
+          <div class="footer-widget">
+            <div class="container">
+              <div class="row">
+                <div class="col-lg-6">
+                  <div class="footer-content">
+                    <a href="#home"><img src="assets/images/LOGO2 PUTIH.png" alt="logo"></a>
+                    <ul>
+                      <li><a href="https://twitter.com/Anforcom"><i class="lni lni-twitter-original"></i></a></li>
+                      <li><a href="https://www.instagram.com/anforcom/"><i class="lni lni-instagram-original"></i></a></li>
+                      <li><a href="https://www.youtube.com/channel/UCYNWHN4O_8om6FX7nKpE-rw"><i class="lni lni-youtube"></i></a></li>
+                    </ul>
+                  </div>
+                </div>
+                <div class="col-lg-6">
+                  <h3>Hubungi kami</h3>
+                  <p class="text">Panitia Anforcom 2020<br>Fakultas Sains dan Matiematika, Universitas Diponegoro,<br>Jl.Prof.Soedharto, S.H.,Tembalang, Semarang 50275 </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </footer>
+        <!-- FOOTER ENDS -->
     </body>
 
     </html>
