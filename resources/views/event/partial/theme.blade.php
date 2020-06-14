e6cd6ccaa6f9f692227be10b0af8f7514655bf3c
Ranu Dwi Setiadi <ranudwis@gmail.com>
2020-06-15 00:34:09 +0700

    Ganti tema

---
+++ resources/views/event/partial/theme.blade.php
@@ -0,0 +1,12 @@
+<section id="tema" class="tema-area">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-lg-6 col-md-10">
+                <div class="section-title text-center pb-10">
+                    <h3 class="title">Tema</h3>
+                    <p class="text">"Reconnecting Society with Advanced Technology"</p>
+                </div> <!-- section title -->
+            </div>
+        </div> <!-- row -->
+    </div> <!-- container-->
+</section><!-- section -->
--- resources/views/event/softdev.blade.php
+++ resources/views/event/softdev.blade.php
@@ -32,22 +32,7 @@
 
 <!-- HOME ENDS -->
 
-<!-- TEMA START -->
-
-<section id="tema" class="tema-area">
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-lg-6 col-md-10">
-                <div class="section-title text-center pb-10">
-                    <h3 class="title">Tema</h3>
-                    <p class="text">"Be a Brilliant Generation Through Digital Tech Innovation!"</p>
-                </div> <!-- section title -->
-            </div>
-        </div> <!-- row -->
-    </div> <!-- container-->
-</section><!-- section -->
-
-<!-- TEMA ENDS -->
+@include('event.partial.theme')
 
 @include('event.partial.timeline')
 
