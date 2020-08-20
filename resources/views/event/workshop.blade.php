15d293ca2dd8485f8be0001ac44c6f99ecf0bfa1
Ranu Dwi Setiadi <ranudwis@gmail.com>
2020-08-20 22:30:13 +0700

    Perbaikan workshop

--- resources/views/event/workshop.blade.php
+++ resources/views/event/workshop.blade.php
@@ -12,9 +12,7 @@
                 <div class="row">
                     <div class="col-lg-6 margin">
                         <h1 class="title">{{ $event->name }}</h1>
-                        <p class="text">Dapatkan materi mulai dari langkah-langkah merilis startup mulai dari nol, hingga mendapatkan feedback dari mentor/speaker terkait startup atau ide kamu.</p>
-                        <ul class="btn"><li><a class="main-btn btn-one" href="{{ route('enroll.show', ['event' => $event->slug]) }}">Daftar</a></li></ul>
-                        <ul class="btn"><li><a class="main-btn btn-two" href="{{ asset('rulebooks/Guidebook Workshop Kickstart Your Startup 101.pdf') }}">Guidebook</a></li></ul>
+                        <p class="text">Dalam perbaikan...</p>
                     </div>
                     <div class="col-lg-6">
                         <div class="img-container">
